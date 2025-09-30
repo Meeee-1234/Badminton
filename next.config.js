2f99b645892c30d53228661237ceab2a59d2fdc8
Bow <thanchanok12sop@gmail.com>
2025-10-01 02:10:15 +0700

    revert: disable static export to keep API/SSR

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  // ไม่มี output: 'export' อีกแล้ว
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
+
 module.exports = nextConfig;
